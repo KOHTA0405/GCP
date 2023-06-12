0959319dd2494b8fb1139bda005d4e171ba99ef0
KOHTA0405 <koh.yoshi.0405@gmail.com>
2023-06-12 15:40:00 +0900

    schema定義とautodetectの無効化

--- cloud_functions/lake_to_dwh/hoge/main.py
+++ cloud_functions/lake_to_dwh/hoge/main.py
@@ -27,8 +27,15 @@ def load_data(request):
     client = bigquery.Client()
     dataset_ref = client.dataset(dataset_id)
     # Set Load Config
-    job_config = bigquery.LoadJobConfig()
-    job_config.autodetect = True
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("id", "integer"),
+            bigquery.SchemaField("name", "string"),
+            bigquery.SchemaField("quantity", "numeric"),
+        ],
+        skip_leading_rows=1,
+    )
+    job_config.autodetect = False
     job_config.source_format = bigquery.SourceFormat.CSV
     job_config.write_disposition = "WRITE_APPEND"
 
